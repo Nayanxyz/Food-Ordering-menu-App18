d526f94acb3a00ad7a7247a5665c420d39643e35
NayanPython <choudharyji527@gmail.com>
2026-04-02 09:25:04 +0530

    added search fields to search meals

--- restaurant_menu/admin.py
+++ restaurant_menu/admin.py
@@ -4,6 +4,6 @@ from .models import Item
 class MenuItemAdmin(admin.ModelAdmin):
     list_display = ("meal", "status")
     list_filter = ("status",)
-
+    search_fields = ("meal", "description")
 
 admin.site.register(Item, MenuItemAdmin)
