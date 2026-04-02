e0a16495a7a28243ea7ba079b4b0faba5b62f0bc
NayanPython <choudharyji527@gmail.com>
2026-04-02 09:23:44 +0530

    added admin to supervise or add items on the webpage by admin interface

--- restaurant_menu/admin.py
+++ restaurant_menu/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Item
 
-# Register your models here.
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ("meal", "status")
+
+
+admin.site.register(Item, MenuItemAdmin)
